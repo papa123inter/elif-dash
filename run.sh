1e1f69372836f48be1ad165d5a4dbe39530b27bd
Sanehka <goodbaan28@gmail.com>
2026-04-15 10:29:07 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Проверяем наличие зависимостей и запускаем
 if ! python3 -c "import pygame" &> /dev/null; then
-    echo "Ошибка: Pygame не установлен. Попробуйте: sudo pacman -S python-pygame"
+    echo "sudo pacman -S python-pygame"
     exit 1
 fi
 python3 game.txt
